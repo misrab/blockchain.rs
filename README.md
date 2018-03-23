8cc7e78cb2adc88a66f1bcb647516dd393d10403
misrab <faizullah.misrab@gmail.com>
2018-03-23 18:01:30 +0800

    initial musing

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # blockchain.rs
 a baby implementation of the Ethereum Yellow Paper, for pedagogical purposes
+
+see https://ethereum.github.io/yellowpaper/paper.pdf
---
+++ src/lib.rs
@@ -0,0 +1,70 @@
+
+
+
+
+
+
+
+type Nonce = u64;
+type Wei = u64;
+
+
+
+struct Account {
+  nonce: Nonce,
+  balance: Wei,
+  //storageRoot: ,
+  //codeHash: ,
+}
+
+
+struct Transaction {
+  nonce: Nonce,
+  gasPrice: Wei,
+  gasLimit: Wei,
+  to: ,
+  value: ,
+  //v, r, s: ,
+  //init || data: Option?
+
+}
+
+
+struct BlockHeader {
+  //parentHash: ,
+  //ommersHash: ,
+  //beneficiary: ,
+  //stateRoot: ,
+  //transactionsRoot: ,
+  //receiptsRoots: ,
+  //logsBloom: ,
+  //difficulty: ,
+  //number: ,
+  //gasLimit: ,
+  //gasUsed: ,
+  //timestamp: ,
+  //extraData: ,
+  //mixHash: ,
+  //nonce: ,
+}
+// also B_U and B_T (block ommers, block transactions)
+
+
+
+
+
+
+
+
+
+
+
+
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
